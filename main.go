afa59bb868c4ba48e64406fe101181efe094b3f7
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-12-10 18:24:06 +0100

    Make key value fsm and setup function

--- main.go
+++ main.go
@@ -2,22 +2,69 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"path"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/hashicorp/raft"
 )
 
-func Apply() {
+type kvFsm struct {
+	v map[string]string
+}
+
+type snapshotNoop struct{}
+
+func (sn snapshotNoop) Persist(_ raft.SnapshotSink) error { return nil }
+func (sn snapshotNoop) Release()
+
+func (kf *kvFsm) Apply(log *raft.Log) any {
 	fmt.Println("Apply")
+	return true
 }
 
-func Read() {
-	fmt.Println("Read")
-}
-
-func Restore() {
+func (kf *kvFsm) Restore(io.ReadCloser) error {
 	fmt.Println("Restore")
+	return nil
+}
+
+func (kf *kvFsm) Snapshot() (raft.FSMSnapshot, error) {
+	return snapshotNoop{}, nil
+}
+
+func setupRaft(dir string, nodeId int, raftAddress string, kvFsm *kvFsm) (*raft.Raft, error) {
+	snapshots, err := raft.NewFileSnapshotStore(path.Join(dir, "snapshot"), 2, os.Stderr)
+	if err != nil {
+		return nil, fmt.Errorf("Could not create snapshot store: %s", err)
+	}
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", raftAddress)
+	if err != nil {
+		return nil, fmt.Errorf("Could not resolve address: %s", err)
+	}
+
+	transport, err := raft.NewTCPTransport(raftAddress, tcpAddr, 10, time.Second*10, os.Stderr)
+	if err != nil {
+		return nil, fmt.Errorf("Could not create tcp transport: %s", err)
+	}
+
+	config := raft.DefaultConfig()
+	config.LocalID = raft.ServerID(nodeId)
+
+	r, err := raft.NewRaft(config, kvFsm, logs, store, snapshots, transport)
+	if err != nil {
+		return nil, fmt.Errorf("Could not create raft instance: %s", err)
+	}
+
+	return r, nil
+}
+
+func getConfig() {
 }
 
 func get(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +86,13 @@ func removeNode(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	dataDir := "data"
+
+	r, err := setupRaft(path.Join(dataDir, "raft"+config.id), config.id, "localhost:9090", kf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := mux.NewRouter()
 	router.HandleFunc("/{key}", get).Methods("GET")
 	router.HandleFunc("/", set).Methods("PUT")
