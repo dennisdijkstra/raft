68b2d0661f2703bb2c17261ca88f924ce5b96d59
Dennis Dijkstra <mail@dennisdijkstra.com>
2023-12-12 17:26:04 +0100

    Attach methods to httpServer struct

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 type kvFsm struct {
-	v map[string]string
+	db *map[string]string
 }
 
 type snapshotNoop struct{}
@@ -24,21 +24,22 @@ type snapshotNoop struct{}
 func (sn snapshotNoop) Persist(_ raft.SnapshotSink) error { return nil }
 func (sn snapshotNoop) Release()
 
-func (kf *kvFsm) Apply(log *raft.Log) any {
+func (kv *kvFsm) Apply(log *raft.Log) any {
+
 	fmt.Println("Apply")
 	return true
 }
 
-func (kf *kvFsm) Restore(io.ReadCloser) error {
+func (kv *kvFsm) Restore(io.ReadCloser) error {
 	fmt.Println("Restore")
 	return nil
 }
 
-func (kf *kvFsm) Snapshot() (raft.FSMSnapshot, error) {
+func (kv *kvFsm) Snapshot() (raft.FSMSnapshot, error) {
 	return snapshotNoop{}, nil
 }
 
-func setupRaft(dir string, nodeId int, raftAddress string, kvFsm *kvFsm) (*raft.Raft, error) {
+func setupRaft(dir string, nodeId string, raftAddress string, kvFsm *kvFsm) (*raft.Raft, error) {
 	config := raft.DefaultConfig()
 	config.LocalID = raft.ServerID(nodeId)
 
@@ -52,12 +53,12 @@ func setupRaft(dir string, nodeId int, raftAddress string, kvFsm *kvFsm) (*raft.
 		return nil, fmt.Errorf("Could not create snapshot store: %s", err)
 	}
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp", raftAddress)
+	tcpAddress, err := net.ResolveTCPAddr("tcp", raftAddress)
 	if err != nil {
 		return nil, fmt.Errorf("Could not resolve address: %s", err)
 	}
 
-	transport, err := raft.NewTCPTransport(raftAddress, tcpAddr, 10, time.Second*10, os.Stderr)
+	transport, err := raft.NewTCPTransport(raftAddress, tcpAddress, 10, time.Second*10, os.Stderr)
 	if err != nil {
 		return nil, fmt.Errorf("Could not create tcp transport: %s", err)
 	}
@@ -70,40 +71,51 @@ func setupRaft(dir string, nodeId int, raftAddress string, kvFsm *kvFsm) (*raft.
 	return r, nil
 }
 
-func getConfig() {
+type httpServer struct {
+	r  *raft.Raft
+	db *map[string]string
 }
 
-func get(w http.ResponseWriter, r *http.Request) {
+func (hs httpServer) get(w http.ResponseWriter, r *http.Request) {
 	key := mux.Vars(r)["key"]
 	fmt.Println(key)
 }
 
-func set(w http.ResponseWriter, r *http.Request) {
+func (hs httpServer) set(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Set key value pair")
 }
 
-func listNodes(w http.ResponseWriter, r *http.Request) {
+func (hs httpServer) listNodes(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("List nodes")
 }
 
-func removeNode(w http.ResponseWriter, r *http.Request) {
+func (hs httpServer) removeNode(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	fmt.Println(id)
 }
 
 func main() {
-	dataDir := "data"
+	nodeId := os.Args[1]
+	if nodeId == "" {
+		log.Fatal("Missing required parameter: --node-id")
+	}
 
-	r, err := setupRaft(path.Join(dataDir, "raft"+config.id), config.id, "localhost:9090", kf)
+	dataDir := "data"
+	db := &map[string]string{}
+	kv := &kvFsm{db}
+
+	r, err := setupRaft(path.Join(dataDir, "raft"+nodeId), nodeId, "localhost:9090", kv)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	hs := httpServer{r, db}
+
 	router := mux.NewRouter()
-	router.HandleFunc("/{key}", get).Methods("GET")
-	router.HandleFunc("/", set).Methods("PUT")
-	router.HandleFunc("/nodes/list", listNodes).Methods("GET")
-	router.HandleFunc("/nodes/{id}", removeNode).Methods("DELETE")
+	router.HandleFunc("/{key}", hs.get).Methods("GET")
+	router.HandleFunc("/", hs.set).Methods("PUT")
+	router.HandleFunc("/nodes/list", hs.listNodes).Methods("GET")
+	router.HandleFunc("/nodes/{id}", hs.removeNode).Methods("DELETE")
 
 	http.Handle("/", router)
 
